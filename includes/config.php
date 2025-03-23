96f7c8be700089ab68fdacfb4484d1d3e9bf3d2c
Dinesh Kumar <mapmydestination@gmail.com>
2025-03-23 11:01:16 +0530

    update

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,9 @@
 <?php
 $config['db']['host'] = 'localhost';
-$config['db']['name'] = 'quickjobs';
+$config['db']['name'] = 'jobclass';
 $config['db']['user'] = 'root';
 $config['db']['pass'] = '';
 $config['db']['pre'] = 'job_';
 
 $config['version'] = '6.1';
-$config['installed'] = '1';
 ?>
