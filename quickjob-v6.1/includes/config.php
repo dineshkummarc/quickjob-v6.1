a657507fe00ac1506850eb3e70e9635c6b6c5e30
Dinesh Kumar <mapmydestination@gmail.com>
2025-03-23 10:40:21 +0530

    update

---
+++ quickjob-v6.1/includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'quickjobs';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'job_';
+
+$config['version'] = '6.1';
+$config['installed'] = '1';
+?>
